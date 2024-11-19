9d0bc6ce0f0271231b15c1a4ecea85f402e368ce
devmc-ee <dev@milicity.eu>
2024-11-19 13:56:36 +0200

    add address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,11 @@ export default function Home() {
           height={300}
           priority
         />
+
+        <div style={{ padding: 10, textAlign: 'center'}}>
+          <p>Viru 9, 2 floor,</p>
+          <p>TALLINN, ESTONIA</p>
+        </div>
     </div>
   );
 }
