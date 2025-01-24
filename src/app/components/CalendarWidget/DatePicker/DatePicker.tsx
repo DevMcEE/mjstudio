a19f063f4d0a28b40953330e093ea0043849519d
synd4772 <doe481157@gmail.com>
2025-01-24 19:07:56 +0200

    MJS-29: Renaming has completed

--- src/app/components/CalendarWidget/DatePicker/DatePicker.tsx
+++ src/app/components/CalendarWidget/DatePicker/DatePicker.tsx
@@ -12,28 +12,28 @@ export const DatePicker = () => {
     const [selectedCellId, setSelectedCellId] = useState<number>(0);
     const currentDate: DateTime = DateTime.now();
     const format = 'yyyy-MM-dd';
-    const [cellsMap, setCellsMap] = useState<cell[]>(dayCellsMapsClient.getRowForView());
+    const [cellsList, setCellsList] = useState<cell[]>(dayCellsMapsClient.getRowForView());
 
     useEffect(()=>{
         EventEmitterClient.emit("OnDaySelect", {id: selectedCellId, date: dayCellsMapsClient.cellsMap[selectedCellId].date});
     }, [selectedCellId])
     useEffect(() => {
         EventEmitterClient.on("DatePickerRowUpdated", () => {
-            setCellsMap(dayCellsMapsClient.getRowForView());
+            setCellsList(dayCellsMapsClient.getRowForView());
         })
         return () => {
             EventEmitterClient.unsubscribe("DatePickerRowUpdated");
         }
     },[])
     useEffect(()=>{
-        console.log(cellsMap, "current row");
-    }, [cellsMap])
+        console.log(cellsList, "current row");
+    }, [cellsList])
 
     return (
         <div className={styles.dataPickerContainer}>
             <button onClick={() => dayCellsMapsClient.scrollTo("left")} className={`${styles.arrowButton} ${styles.leftArrowButton}`}>&lsaquo;</button>
                 <div className={styles.cellsContainer}>
-                    {cellsMap.map((item, index) => {
+                    {cellsList.map((item, index) => {
                         return (
                             <DayCell key={index} onClick={()=>setSelectedCellId(item.id)} isCurrentDay={item.date.toFormat(format) === currentDate.toFormat(format)} cellId={item.id} isSelected={item.id === selectedCellId} date={item.date} isUnavailable={item.date !<= currentDate && (item.date.toFormat(format)===currentDate.toFormat(format) ? false : true)}/>
                         )
