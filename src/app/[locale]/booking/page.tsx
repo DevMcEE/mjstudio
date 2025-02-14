05077771d3a19040ab887400306e4c09b7daed6d
Roman Lavrunov <lavrunov@gmail.com>
2025-02-14 18:58:12 +0200

    MJS-37:Fix minor issues

--- src/app/[locale]/booking/page.tsx
+++ src/app/[locale]/booking/page.tsx
@@ -2,12 +2,20 @@ import { Stepper } from "@/app/components/Stepper/Stepper";
 import styles from "./booking.module.css";
 import { useLocale, useTranslations } from "next-intl";
 import { Locale } from "@/i18n/config.types";
-import { Translations } from "@/app/components/Stepper/Stepper.types";
-import { steps } from "@/app/components/Stepper/steps";
+import { FormStep, Translations } from "@/app/components/Stepper/Stepper.types";
+import { ServiceSelectionForm } from "@/app/components/ServiceSelectionForm/ServiceSelectionForm";
+import { MockForm } from "@/app/components/Stepper/StepperMockData";
 
-export default function HomePage() {
+export default function BookingPage() {
   const t = useTranslations("booking");
   const locale = useLocale() as Locale;
+
+  const steps: FormStep[] = [
+    { name: "service", component: ServiceSelectionForm, showServiceInActionBar: true },
+    { name: "date_time", component: MockForm, showServiceInActionBar: false },
+    { name: "contacts", component: MockForm, showServiceInActionBar: false },
+    { name: "finish", component: MockForm, showServiceInActionBar: false },
+  ];
 
   const translations: Translations = {
     stepsTranslationsMap: {
@@ -27,7 +35,7 @@ export default function HomePage() {
         <h1>{t("booking")}</h1>
       </div>
       <div className={styles.stepper}>
-        <Stepper translations={translations} locale={locale} steps={steps}/>
+        <Stepper translations={translations} locale={locale} steps={steps} />
       </div>
     </div>
 
